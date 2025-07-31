3e06186397f5f4be89d84ea40c75bc39d87700d8
SpruceParsley80 <slugbug427@gmail.com>
2025-07-31 16:16:59 -0700

    the ascii art is fixed!1!!1111!!1!

--- TheTaleOfTheMysticalInspections/src/utility.cpp
+++ TheTaleOfTheMysticalInspections/src/utility.cpp
@@ -15,23 +15,24 @@ bool utility::waitForEnter() {
                     }
                 }
 void utility::printAsciiArt(int choice) {
-    string output;
+    string output = "DEFAULT VALUE IF YOU SEE THIS SOMETHING IS WRONG";
     ifstream inputFile("/Users/masonsacksteder/TheTaleOfTheMysticalInspections-main/TheTaleOfTheMysticalInspections/ascii-art.txt");
-    inputFile.open("/Users/masonsacksteder/TheTaleOfTheMysticalInspections-main/TheTaleOfTheMysticalInspections/ascii-art.txt");
+    // inputFile.open("/Users/masonsacksteder/TheTaleOfTheMysticalInspections-main/TheTaleOfTheMysticalInspections/ascii-art.txt");
     cout << "if statement reacxhed";
     if (inputFile.is_open()) {
         cout << "for loop reached";
-        while (getline(inputFile, output)) {
-            cout << output;
-        }
-        // for (int i = 0; i < choice * 22; i++) {
-        //     getline(inputFile, output);
+        // while (getline(inputFile, output)) {
+        //     cout << output;
         // }
-        //     for (int i = 0; i < 22; i++) {
-        //         getline(inputFile, output);
-        //         cout << "Line " << i << " Printed" << endl;
-        //         cout << output << endl;
-        //     }
+        for (int i = 0; i < choice * 22; i++) {
+            getline(inputFile, output);
+        }
+            for (int i = 0; i < 22; i++) {
+                getline(inputFile, output);
+                // cout << "Line " << i << " Printed" << endl;
+                cout << output << endl;
+            }
+            inputFile.close();
         }
 
 }
